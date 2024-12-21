c118df09e4ca1d3d8a48270dffbf9c45b64f4e27
chizh04 <chizh04@bk.ru>
2024-12-21 21:13:48 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,6 @@
+created_date = "20-12-2024"
+issue_date = "25-12-2024"
+temp_created_date = created_date[:5]
+temp_issue_date = issue_date[:5]
+print(temp_created_date)
+print(temp_issue_date)
---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Дмитрий"
+title = "Заголовок заметки"
+content = "Описание заметки"
+status = "Статус заметки"
+created_date = "20-12-2024"
+issue_date = "25-12-2024"
+
+print("Имя пользователя", username)
+print("Заголовок заметки", title)
+print("Описание заметки", content)
+print("Статус заметки", status)
+print("Дата создания заметки", created_date)
+print("Дэдлайн", issue_date)
