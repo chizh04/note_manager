174f241f750874e696932897a8ea0fd9c90eb2d4
chizh04 <chizh04@bk.ru>
2025-01-06 11:38:27 +0300

    Add files via upload

--- update_status.py
+++ update_status.py
@@ -1,9 +1,33 @@
-from add_titles_loop import add_titles
+input_status = ["Отложено"] # статус заметки. Меняется в результате работы программы на основе ввода данных пользователем
 
-non_titles = []
-status = "Пустая заметка"
+dict_status = ["Отложено", "Выполнено", "В работе"] # Возможные варианты статуса заметки.
 
-if non_titles != add_titles:
-    status = "В работе"
+print("Текущий статус заметки:", input_status[0], "\n")
+print("Возможные статусы заметки: \n"
+      " 1. Отложено \n"
+      " 2. Выполнено \n"
+      " 3. В работе \n")
 
-print("Статус: ", status)
+# Функционал для изменения пользователем статуса заметки:
+while True:
+    status = input("Введите новый статус заметки из возможных (или его порядковый номер): ")
+    if status == "1" or status == "Отложено" or status == "отложено":
+        status_ = dict_status[0]
+        input_status[0] = status_
+        break
+    elif status == "2" or status == "Выполнено" or status == "выполнено":
+        status_ = dict_status[1]
+        input_status[0] = status_
+        break
+    elif status == "3" or status == "В работе" or status == "в работе":
+        status_ = dict_status[2]
+        input_status[0] = status_
+        break
+    else:
+        input("Недопустимый статус заметки, повторите ввод: ")
+
+actual_status = {"status" : input_status[0]}
+
+# Вывод нового статуса заметки:
+print("Статус заметки успешно изменен на: ")
+print(actual_status["status"])
